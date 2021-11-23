fee1e9718bcfe2ee5fb62fb80f509c03fd88cd56
Cancu <juancruz.cancu@gmail.com>
2021-11-23 16:37:09 -0300

    API WORKING

---
+++ taskieApp/serializers.py
@@ -0,0 +1,15 @@
+### serializers
+
+from rest_framework import serializers
+from taskieApp.models import Departments,Employees
+
+
+class DepartmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Departments
+        fields=('DepartmentId', 'DepartmentName')
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Employees
+        fields=('EmployeeId', 'EmployeeName', 'Department', 'DateOfJoining')
--- taskieApp/views.py
+++ taskieApp/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from django.http.response import JsonResponse
+
+from taskieApp.models import Departments,Employees
+from taskieApp.serializers import DepartmentSerializer,EmployeeSerializer
+
 
 # Create your views here.
+
+@csrf_exempt
+
+def departmentApi(req, id=0):
+    if req.method == 'GET':
+        departments = Departments.objects.all()
+        departments_serializer = DepartmentSerializer(departments, many=True)
+        return JsonResponse(departments_serializer.data, safe=False)
+    elif req.method == 'POST':
+        department_data = JSONParser().parse(req)
+        departments_serializer = DepartmentSerializer(data = department_data)
+        if departments_serializer.is_valid():
+            departments_serializer.save()
+            return JsonResponse("Added Succesfully", safe=False)
+        return JsonResponse("Failed to Add", safe=False)
+    elif req.method == 'PUT':
+        department_data = JSONParser().parse(req)
+        department = Departments.objects.get(DepartmentId = department_data['DepartmentId'])
+        departments_serializer = DepartmentSerializer(department, data=department_data)
+        if departments_serializer.is_valid():
+            departments_serializer.save()
+            return JsonResponse("actulizado!!!!!!!", safe= False)
+        return JsonResponse("Fallo")
+    elif req.method == 'DELETE':
+        department = Departments.objects.get(DepartmentId = id)
+        department.delete()
+        return JsonResponse("Deleted succesfuly", safe = False)
+         
+
+
+
+
+
+
+
